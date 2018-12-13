3acc591558a5308516c1dfa4102b997e7b021809
Taufik Nurrohman <t.nurrohman77@gmail.com>
2018-12-13 22:19:24 +0700

    Revert to Classic Tags Style

--- bloggerable/page.tags.php
+++ bloggerable/page.tags.php
@@ -1,13 +1,13 @@
 <?php
 
 if (Extend::exist('tag')) {
-    echo '<div class="post-label"><ul class="post-label-list">';
+    echo '<ul class="post-label">';
     call_user_func(function() {
         extract(Lot::get());
         $tags = $page->tags->map(function($tag) {
             return '<li class="post-label-item">' . HTML::a($tag->title, $tag->url, false, ['class[]' => ['post-label-link'], 'rel' => 'tag']) . '</li>';
         });
-        echo $tags->count ? $tags("") : '<li class="post-label-item"><span class="post-label-link nolabel">Unlabelled</span></li>';
+        echo $tags->count ? $tags("") : '<li class="post-label-item"><span class="post-label-link nolabel">' . $language->_tag . '</span></li>';
     });
-    echo '</ul></div>';
+    echo '</ul>';
 }
